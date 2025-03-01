ea8200e14b44ad322b17222f722c5b7bfc0722d0
Dhanan20jeyan122003 <kmayan1967@gmail.com>
2025-03-01 15:46:42 +0530

    Added .env to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-mongoose.connect('mongodb://127.0.0.1:27017/userdb', {
+
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
