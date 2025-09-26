9bd0766c1c1eec2f4708bc4fdb7aa2510847edab
DHANANJEYAN M <146374768+Dhanan20jeyan122003@users.noreply.github.com>
2025-09-26 08:09:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 const path = require('path');
 
-const mongoUri = process.env.MONGO_URI;
 
-mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
